60d885efcfe59721a3bef46fadf9b80a5f12bc8e
carlosdizx <carlodiaz@umariana.edu.co>
2021-04-30 10:16:24 -0500

    Creamos la entidad persona, ademas definimos los atributos de conexion a la base de datos

---
+++ src/main/java/simuPremium/models/entity/Persona.java
@@ -0,0 +1,124 @@
+package simuPremium.models.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "personas")
+public class Persona
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long documento;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellidos;
+
+    @Column(nullable = false)
+    private Date fechaNacimiento;
+
+    @Column(nullable = false)
+    private int tipo;
+
+    @Column(nullable = false)
+    private String sangre;
+
+    @Column(nullable = false)
+    private int tipo_estado;
+
+    @Column(nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private long celular;
+
+    public Persona(Long documento, String nombre, String apellidos, Date fechaNacimiento, int tipo, String sangre, int tipo_estado, String correo, long celular) {
+        this.documento = documento;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipo = tipo;
+        this.sangre = sangre;
+        this.tipo_estado = tipo_estado;
+        this.correo = correo;
+        this.celular = celular;
+    }
+
+    public Persona() {
+    }
+
+    public Long getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(Long documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSangre() {
+        return sangre;
+    }
+
+    public void setSangre(String sangre) {
+        this.sangre = sangre;
+    }
+
+    public int getTipo_estado() {
+        return tipo_estado;
+    }
+
+    public void setTipo_estado(int tipo_estado) {
+        this.tipo_estado = tipo_estado;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(long celular) {
+        this.celular = celular;
+    }
+}
