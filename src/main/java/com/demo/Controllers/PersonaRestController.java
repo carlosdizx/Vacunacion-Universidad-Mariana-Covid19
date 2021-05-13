fc5de11634e8184add9cfb6104add87c498a1805
carlosdizx <carlodiaz@umariana.edu.co>
2021-05-13 13:09:37 -0500

    Se agregaron los metodos para graficar

--- src/main/java/com/demo/Controllers/PersonaRestController.java
+++ src/main/java/com/demo/Controllers/PersonaRestController.java
@@ -18,6 +18,8 @@ import java.util.Map;
 @RequestMapping("/personas")
 public class PersonaRestController
 {
+    final static DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###.##");
+
     @Autowired
     private IPersonaService service;
 
@@ -141,17 +143,16 @@ public class PersonaRestController
             final int administrativos = service.findTiposPersonas(3).size();
             final int directivos = service.findTiposPersonas(4).size();
             final int posibles = service.findPosibles().size();
-            if( total<0 || total!=(estudiantes+docentes+administrativos+directivos) )
+            if( total<=0 || total!=(estudiantes+docentes+administrativos+directivos) )
             {
                 response.put("Mensaje","No hay personas registradas en las bases de datos, o los datos cargados son incorrectos!");
                 return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            final DecimalFormat df = new DecimalFormat("###.##");
-            final String estudiantesPor = df.format((double) 100*estudiantes/total);
-            final String docentesPor = df.format((double)100*docentes/total);
-            final String administrativosPor = df.format((double)100*administrativos/total);
-            final String directivosPor = df.format((double)100*directivos/total);
-            final String posiblesPor = df.format((double)100*posibles/total);
+            final String estudiantesPor = DECIMAL_FORMAT.format((double) 100*estudiantes/total);
+            final String docentesPor = DECIMAL_FORMAT.format((double)100*docentes/total);
+            final String administrativosPor = DECIMAL_FORMAT.format((double)100*administrativos/total);
+            final String directivosPor = DECIMAL_FORMAT.format((double)100*directivos/total);
+            final String posiblesPor = DECIMAL_FORMAT.format((double)100*posibles/total);
             response.put("Total 100%",total);
             response.put("Estudiantes "+estudiantesPor+"%",estudiantes);
             response.put("Docentes "+docentesPor+"%",docentes);
@@ -174,7 +175,32 @@ public class PersonaRestController
         final Map<String,Object> response = new HashMap<>();
         try
         {
-
+            final int total = list().size();
+            final int desconocidos = service.findEstadosPersonas(1).size();
+            final int contagiados = service.findEstadosPersonas(2).size();
+            final int saludabes = service.findEstadosPersonas(3).size();
+            final int preContagiados = service.findEstadosPersonas(4).size();
+            final int preVacunados = service.findEstadosPersonas(5).size();
+            final int vacunados = service.findEstadosPersonas(6).size();
+            if( total<=0 || total!=(desconocidos+contagiados+saludabes+preContagiados+preVacunados+vacunados) )
+            {
+                response.put("Mensaje","No hay personas registradas en las bases de datos, o los datos cargados son incorrectos!");
+                response.put("Mensaje2","Total personas: "+total+", sumatoria: "+(desconocidos+contagiados+saludabes+preContagiados+preVacunados+vacunados));
+                return new ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            final String desconocidosPor = DECIMAL_FORMAT.format((double) 100*desconocidos/total);
+            final String contagiosPor = DECIMAL_FORMAT.format((double) 100*contagiados/total);
+            final String saludablesPor = DECIMAL_FORMAT.format((double) 100*saludabes/total);
+            final String preContagiadosPor = DECIMAL_FORMAT.format((double) 100*preContagiados/total);
+            final String preVacunadosPor = DECIMAL_FORMAT.format((double) 100*preVacunados/total);
+            final String vacunadosPor = DECIMAL_FORMAT.format((double) 100*vacunados/total);
+            response.put("Total 100%",total);
+            response.put("Desconocidos "+desconocidosPor+"%",desconocidos);
+            response.put("Contagiados "+contagiosPor+"%",contagiados);
+            response.put("Saludables "+saludablesPor+"%",saludabes);
+            response.put("Anteriormente contagiados "+preContagiadosPor+"%",preContagiados);
+            response.put("Pendiente segunda dosis "+preVacunadosPor+"%",preVacunados);
+            response.put("Vacunados "+vacunadosPor+"%",vacunados);
             return new ResponseEntity(response, HttpStatus.OK);
         }
         catch (DataAccessException e)
